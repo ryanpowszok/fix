fe683c78c3220d3fff6097d9139e122fd0bd17d4
Ryan Powszok <rpowszok@vsapartners.com>
2017-03-30 22:01:59 -0500

    Add volume cleanup

--- resetDocker.sh
+++ resetDocker.sh
@@ -7,4 +7,6 @@ docker rm $(docker ps -a -q)
 # Delete all images
 docker rmi $(docker images -q)
 
+# Clean up orphan Docker volumes
+# @see - https://github.com/chadoe/docker-cleanup-volumes
 docker volume rm $(docker volume ls -qf dangling=true)
